cf9c22b7b07da28ff2062489993bc5af34417a8b
Imer D <u6038294@umail.utah.edu>
2021-08-20 10:35:51 -0600

    modified index page

--- js/script.js
+++ js/script.js
@@ -101,7 +101,7 @@ fetch('/snippet/paper.json')
 						var mainContainer = document.getElementById("myData");
 						for (var i = 0; i < data.length; i++) {
 							var div = document.createElement("div");
-						    div.innerHTML = ' ' + data[i].date + '* ' + data[i].Name+ '* ' + data[i].link; 
+						    div.innerHTML = ' ' + data[i].Name+ '* ' + data[i].link; 
 							mainContainer.appendChild(div);
 						}
 					}
